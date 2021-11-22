46a127c9d3540addbca2aa4f963786b05c20b6af
Nikita <nicitazaletin@yandex.ru>
2021-11-22 22:26:11 +0300

    UX работает

--- bank_client/src/App.js
+++ bank_client/src/App.js
@@ -1,27 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import { About } from "./comonents/About/About";
 import { Header } from './comonents/Header/Header';
 import { Menu } from "./comonents/Menu/Menu";
 import {TabContent} from './comonents/TabContent/TabContent';
 import { Tables } from "./comonents/Tables/Tables";
-import {Autocomplete, TextField} from '@mui/material/';
+import {Autocomplete, TextField, Slider, Typography, FilledInput } from '@mui/material/';
 import axios from 'axios';
+const { evaluate } = require('mathjs')
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
-function App() {
-  const uriTables = "https://83b0-93-81-207-6.ngrok.io/test"
+const App = () => {
+  const uriTables = "https://37df-93-81-207-6.ngrok.io/?tarifServerName="
 
 const tarifs = [
 { 
-  tarifName:'Тариф 1', 
-  tarifServerName:'Tarif1'
+  tarifName:'Накопительный счёт', 
+  tarifServerName:'savings_acc'
 },
 { 
-  tarifName:'Тариф 2', 
-  tarifServerName:'Tarif2'
+  tarifName:'Вклад для студентов', 
+  tarifServerName:'contribution_for_students'
+}
+,
+{ 
+  tarifName:'Вклад для военных', 
+  tarifServerName:'contribution_for_the_military'
 }
 ];
+
+const [formula, setFormula] = useState("y*x")
+const [yearValue, setYearValue] = useState(1)
+const [moneyValue, setMoneyValue] = useState(1000)
+const [profit, setProfit] = useState(1000)
+
+//x - количество денег, y - количество лет
+const getResult = (x,y) => Math.floor(evaluate(formula, {x:x,y:y}))
+
 const testChange = (event, value) =>{
 
   let tarifServetValue = "";
@@ -31,16 +46,71 @@ const testChange = (event, value) =>{
     }
   }
 
-  console.log(tarifServetValue)
-
-  axios.get(uriTables).then((resp) =>{
-    console.log(resp)
+  axios.get(uriTables + tarifServetValue).then((resp) =>{
+    setFormula(resp.data)
+    setProfit(getResult(moneyValue, yearValue))
   })
 }
 
+//Заработок
+useEffect(()=>{
+  setProfit(getResult(moneyValue, yearValue))
+},[yearValue, moneyValue, formula])
+
+
+//Сумма вклада
+const handleChangeMoney = (event) => {
+
+  const data = event.target.value;
+  if(data.length <= 7 && (/^[0-9]+$/.test(data) || data.length===0)){
+    setMoneyValue(event.target.value)
+  }
+  // if(/^[0-9]+$/.test(data)){
+  //   let number = Number(data)
+  //   console.log(number)
+
+  //   if(number>=1000 && number<=1000000){
+  //     setMoneyValue(event.target.value)
+  //   }
+  // }
+  // const result = /^[0-9]+$/.test(data)
+  // console.log(result)
+
+  // setMoneyValue(event.target.value);
+  // console.log(event.target.value)
+  // if (typeof event.target.value === 'number') {
+  //   console.log(event.target.value)
+  //   setMoneyValue(event.target.value);
+  // }
+};
+
+//Года
+const handleChangeYears = (event, newValue) => {
+  if (typeof newValue === 'number') {
+    setYearValue(newValue);
+  }
+};
+const valueLabelFormat = (value) => {
+  const units = ['Год', 'Года', 'Лет'];
+
+  let unitIndex = 0;
+  let scaledValue = value;
+
+  if(value === 1){
+    unitIndex = 0
+  }else if(value<=4){
+    unitIndex = 1
+  }
+  else{
+    unitIndex = 2;
+  }
+
+  return `${scaledValue} ${units[unitIndex]}`;
+}
 
   return (
     <div className="App">
+
       <Autocomplete
         disablePortal
         id="combo-box-demo"
@@ -49,6 +119,25 @@ const testChange = (event, value) =>{
         onInputChange={testChange}
         renderInput={(params) => <TextField {...params} label="Movie" />}
       />
+
+      <TextField  id="filled-basic" label="Filled" variant="filled" onChange={handleChangeMoney} value={moneyValue}/> 
+
+      <Typography id="non-linear-slider" gutterBottom>
+        На срок: {valueLabelFormat(yearValue)}
+      </Typography>
+      <Slider
+        value={yearValue}
+        min={1}
+        step={1}
+        max={10}
+        // getAriaValueText={valueLabelFormat}
+        // valueLabelFormat={valueLabelFormat}
+        onChange={handleChangeYears}
+        valueLabelDisplay="auto"
+        aria-labelledby="non-linear-slider"
+      />
+
+    <TextField  id="filled-basic" label="Filled" variant="filled" disabled value={profit}/> 
     </div>
   );
 }
