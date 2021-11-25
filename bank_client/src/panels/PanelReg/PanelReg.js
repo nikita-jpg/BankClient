c3f22e910eca778caae074742d7381c517ee9111
Nikita <nicitazaletin@yandex.ru>
2021-11-25 17:13:23 +0300

    ФИО поля

--- bank_client/src/panels/PanelReg/PanelReg.js
+++ bank_client/src/panels/PanelReg/PanelReg.js
@@ -1,4 +1,4 @@
-import { Autocomplete, Slider, TextField, Typography } from "@mui/material";
+import { Autocomplete, Button, Slider, TextField, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import TextFieldWrapper from "../../components/TextFieldWrapper/TextFieldWrapper";
 import PanelBlank from "../PanelBlank/PanelBlank";
@@ -6,15 +6,32 @@ import './PanelReg.css'
 
 const PanelReg = ({}) => {
 
+    const [nameValue, setNameValue] = useState("")
+    const [familiaValue, setFamiliaValue] = useState("")
+    const [otchestvoValue, setOtchestvoValue] = useState("")
+
+    //Обработчики
+    const checkFIOValue = (event) => {
+        const data = event.target.value;
+        return true
+    }
+
+
     return(
         <div className="PanelReg">
             <div className="PanelReg__title">Закажите звонок</div>
             <div className="PanelReg_container">
+                {/* ФИО */}
+                <TextFieldWrapper onChange={(event)=>{if(checkFIOValue(event)){setNameValue(event.target.value)}}} value={nameValue} label="Имя"/>
+                <TextFieldWrapper onChange={(event)=>{if(checkFIOValue(event)){setFamiliaValue(event.target.value)}}} value={familiaValue} label="Фамилия"/>
+                <TextFieldWrapper onChange={(event)=>{if(checkFIOValue(event)){setOtchestvoValue(event.target.value)}}} value={otchestvoValue} label="Отчество"/>
+
+                {/* Телефон */}
                 <TextFieldWrapper label="Введите сумму вклада"/>
                 <TextFieldWrapper label="Введите сумму вклада"/>
                 <TextFieldWrapper label="Введите сумму вклада"/>
                 <TextFieldWrapper label="Введите сумму вклада"/>
-                <TextFieldWrapper label="Введите сумму вклада"/>
+                <Button>Отпарвить заявку</Button>
             </div>
         </div>
     )
